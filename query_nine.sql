4a14b326e6540661857ae3c717b17cdc2555a528
Jack Yu <jyu108@jhu.edu>
2021-05-10 12:22:19 -0400

    query nine html

--- query_nine.sql
+++ query_nine.sql
@@ -2,12 +2,12 @@ DELIMITER //
 
 DROP PROCEDURE IF EXISTS query_nine //
 
-CREATE PROCEDURE query_nine()
+CREATE PROCEDURE query_nine(IN season_input VARCHAR(4))
 BEGIN
         with best_team AS (
                 SELECT W / (W + L) as win_percentage, TEAM, END_SEASON as SEASON
                 FROM Team_Misc_Info
-                WHERE END_SEASON = 2018 and W = (Select Max(W) from Team_Misc_Info WHERE END_SEASON = 2018)
+                WHERE END_SEASON = season_input and W = (Select Max(W) from Team_Misc_Info WHERE END_SEASON = season_input)
         ),
         best_team_roster AS (
                 select *
