6620f80f117188e396e4b4823b24d1c2c1c9b90c
Jack Yu <jyu108@jhu.edu>
2021-05-10 19:43:52 -0400

    data visualization for query 11

--- query_eleven.php
+++ query_eleven.php
@@ -4,24 +4,70 @@
     include 'open.php';
     echo "<br>";
     $query = "CALL query_eleven();";
-    if ($result = $conn->query($query)) {
-        if (mysqli_num_rows($result) == 0) {
-            echo "No Results";
-        } else {
-            echo "<h2> Results: </h2>";
-            echo "<table border = \"5px solid black\">";
-            echo "<tr><th> Season </th><th> Avg PPG </th></tr>";
-            foreach($result as $row) {
-                echo "<tr><td>".$row['SEASON']."</td>";
-                echo "<td>".$row['avg_pts']."</td></tr>";
+    $dataPoints = array();
+    if ($stmt = $conn->prepare($query)) {
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            $result->fetch_assoc();
+            if (mysqli_num_rows($result) == 0) {
+                echo "No Results";
+            } else {
+                echo "<h2> Results: </h2>";
+                echo "<table border = \"5px solid black\">";
+                echo "<tr><th> Season </th><th> Avg PPG </th></tr>";
+                foreach($result as $row) {
+                    $temp =  array("x" => strtotime(substr($row["SEASON"], 0, 4).'-1-01') * 1000, 
+                                   "y" => $row["avg_pts"]);
+                    array_push($dataPoints, $temp);
+                    echo "<tr><td>".$row['SEASON']."</td>";
+                    echo "<td>".$row['avg_pts']."</td></tr>";
+                }
+                echo "</table>";
             }
-            echo "</table>";
+        } else {
+        echo "ERROR: Call to query_eleven failed <br>";
         }
     } else {
-        echo "ERROR: Call to query_eleven failed <br>";
+        echo "ERROR: Prepare Failed <br>";
     }
-
     echo "<br>";
+    $stmt->close();
     $conn->close();
 ?>
 </body>
+
+<html>
+<head>
+<script type="text/javascript">
+
+window.onload = function () {
+   var chart = new CanvasJS.Chart("chartContainer",
+   {
+      title: {
+         text: "Average PPG for above 0.500 teams",
+         fontFamily: 'tahoma'
+      },
+         axisY: {
+            title: "Regular Season Points Per Game"
+         },
+         axisX: {
+            title: "Year"
+         },
+         data: [{
+            type: "line",
+            color: "#369EAD",
+            xValueType: "dateTime",
+            dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+         }]
+   });
+
+	chart.render();
+}
+
+</script>
+<script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</head>
+<body>
+<div id="chartContainer" style="height: 500; width: 100%;"></div>
+</body>
+</html>
